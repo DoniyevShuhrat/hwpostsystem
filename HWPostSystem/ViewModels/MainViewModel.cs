6fe1b7e9b521ea5eb7beeddda269d5ac20b39c91
DoniyevShuhrat <doniyevshuhrat96304@gmail.com>
2024-07-18 00:30:46 +0500

    My first commit after rmdir /s /q .vs

---
+++ HWPostSystem/ViewModels/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using HWPostSystem.Models;
+using HWPostSystem.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWPostSystem.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        //Fields
+        private UserAccountModel _currentUserAccount;
+        private IUserRepository userRepository;
+        public UserAccountModel CurrentUserAccount
+        {
+            get { return _currentUserAccount; }
+            set
+            {
+                _currentUserAccount = value;
+                OnPropertyChanged(nameof(CurrentUserAccount));
+            }
+        }
+        public MainViewModel()
+        {
+            userRepository = new UserRepository();
+            CurrentUserAccount = new UserAccountModel();
+            LoadCurrentUserData();
+        }
+
+        private void LoadCurrentUserData()
+        {
+            var user = userRepository.GetByUsername(Thread.CurrentPrincipal.Identity.Name);
+            if (user != null)
+            {
+                CurrentUserAccount.Username = user.Username;
+                CurrentUserAccount.DisplayName = $"Welcome {user.Name} {user.LastName} ;)";
+                CurrentUserAccount.ProfilePicture = null;
+            }
+            else
+            {
+                CurrentUserAccount.DisplayName = "Invalid user, not logged in";
+                //Hide child views.
+            }
+        }
+    }
+}
